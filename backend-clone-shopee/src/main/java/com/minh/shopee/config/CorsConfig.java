06a24be772d2ac4fa4b6dfbe94bf613849bc3fa9
Your Name <you@example.com>
2025-10-10 17:42:47 +0700

    update

--- backend-clone-shopee/src/main/java/com/minh/shopee/config/CorsConfig.java
+++ backend-clone-shopee/src/main/java/com/minh/shopee/config/CorsConfig.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j(topic = "CorsConfiguration")
 public class CorsConfig {
 
-    @Value("${URL_FONTENT}")
+    @Value("${URL_FRONTEND}")
     String urlFontend;
 
     @Bean
